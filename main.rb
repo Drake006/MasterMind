ec99c83ded2d73350e87070c179b7b281b34b694
Drake006 <157472450+Drake006@users.noreply.github.com>
2024-03-19 15:37:52 -0400

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,2 +1,10 @@
 #allows the use of the classes in the classes file
 require_relative 'MstrMnd_classes'
+
+create_game = Create_Game.new
+create_game.rules
+create_game.num_pegs
+create_game.duplicates
+
+play_game = Play_Game.new(create_game)
+play_game.create_sequence
