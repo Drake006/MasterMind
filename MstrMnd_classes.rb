d03a777da1dcbe309e9ace6a9d4ae9ab3aaeb266
Drake006 <157472450+Drake006@users.noreply.github.com>
2024-03-19 15:37:34 -0400

    Update MstrMnd_classes.rb

--- MstrMnd_classes.rb
+++ MstrMnd_classes.rb
@@ -1,17 +1,23 @@
 #this class creates the game, displaying the rules ans asking the user if they want to allow duplicates and how many pegs they would like to use (4, 6 or 8)
 class Create_Game
+  attr_reader :pegs,:dups
   #displays the rules of the game if the user chooses to do so (they may not want to if they already know how to play).
   def rules
-     puts"Welcome to mastermind!!! Here you will try and guess the coreect sequence of colors represented by letters. The list of colors are: \n
-    red='r'  green='g'  blue='b'  yellow='y'/n
-    purple='p'  orange='o'  white='w'  silver='s'/n
-    The guess should be inputted with space seperation like the example below:/n
-    r o s w/n
-    You will also have the choice between guessing 4, 6 or 8 pegs as well as choosing to allow dupliactes of colors to set the difficulty to your liking.\n
-    After each guess, all previous guesses will be displayed and next to those guesses will be a combination of '\u2713's','.' and 'x'. a '\u2713' means \n
-    that you have a color in the right place. A '.' means that you have the right color and an 'x' tells you that you have the wrong color. But be warned\n
-    the order of the combination of \u2713's,.s' and x's isn't the same as the actual sequence of colors. \u2713's will always appear first, then .'s and then x's\n.
-    Once the Game is finished you will have the option to select a play again and choose different paramaters (or the same ones) is you so please. Enjoy :)"
+    print "Would you like to see the rules (y/n)?"
+    if gets.chomp.downcase='y'
+      puts"Welcome to mastermind!!! Here you will try and guess the coreect sequence of colors represented by letters. The list of colors are: \n
+      red='r'  green='g'  blue='b'  yellow='y'/n
+      purple='p'  orange='o'  white='w'  silver='s'/n
+      The guess should be inputted with space seperation like the example below:/n
+      r o s w/n
+      You will also have the choice between guessing 4, 6 or 8 pegs as well as choosing to allow dupliactes of colors to set the difficulty to your liking.\n
+      After each guess, all previous guesses will be displayed and next to those guesses will be a combination of '\u2713's','.' and 'x'. a '\u2713' means \n
+      that you have a color in the right place. A '.' means that you have the right color and an 'x' tells you that you have the wrong color. But be warned\n
+      the order of the combination of \u2713's,.s' and x's isn't the same as the actual sequence of colors. \u2713's will always appear first, then .'s and then x's\n.
+      Once the Game is finished you will have the option to select a play again and choose different paramaters (or the same ones) is you so please. Enjoy :)"
+    else
+      continue
+    end
   end
   
   def num_pegs # asks the user for the number of pegs that they'd like to use. Must either be 4, 6 or 8.
@@ -40,6 +46,15 @@ end
 
 #Play_game contains the functions that randomly selects the colors and stores the users guesses, works with the Display class.
 class Play_Game
+  def initialize(create_game)
+    @create_game=create_game
+  end
+  def colors
+    @colors=['r','g','b','y','p','o','w','s']
+  end
+  def create_sequence
+    pegs=@create_game.pegs
+  end
 end
 
 #The Display class contains all of the functions necessary to show the results of a guesses and previous guesses, goes hand in hand woth Play_Game.
