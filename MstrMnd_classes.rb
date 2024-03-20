1a49a5f53804ff054e345d9b4877f192558ab40a
Drake006 <157472450+Drake006@users.noreply.github.com>
2024-03-20 00:43:58 -0400

    Comments for MstrMnd_classes.rb

--- MstrMnd_classes.rb
+++ MstrMnd_classes.rb
@@ -1,6 +1,6 @@
 #this class creates the game, displaying the rules ans asking the user if they want to allow duplicates and how many pegs they would like to use (4, 6 or 8)
 class Create_Game
-  attr_reader :pegs,:dups
+  attr_reader :pegs,:dups #allows variables to be accessed in Play_Game class
   #displays the rules of the game if the user chooses to do so (they may not want to if they already know how to play).
   def rules
     print "Would you like to see the rules (y/n)?"
@@ -15,12 +15,12 @@ class Create_Game
       puts "Enjoy :)"
     end
   end
-  
+
   def num_pegs # asks the user for the number of pegs that they'd like to use. Must either be 4, 6 or 8.
     loop do
       print"Enter the number of pegs (4, 6 or 8): "
       @pegs=gets.chomp.to_i
-      break if [4, 6, 8].include?(@pegs)
+      break if [4, 6, 8].include?(@pegs) #will continue ti ask the user until given a valid input
     end
   end
   
@@ -44,66 +44,70 @@ end
 
 #Play_game contains the functions that randomly selects the colors and stores the users guesses.
 class Play_Game
-  attr_reader :sequence,:all_guesses
-  def initialize(create_game)
+  attr_reader :sequence,:all_guesses #allows variables to be accessed by display class.
+  
+  def initialize(create_game) #initializes by using create_game instance while initializing the all_guesses array and max_guesses variable
     @create_game=create_game
-    @all_guesses=[]
+    @all_guesses=[] #array that will store every guess.
     @max_guesses = 10
   end
-  def colors
+  
+  def colors #makes the list of the possible colors.
     ['r','g','b','y','p','o','w','s']
   end
-  def create_sequence
+  
+  def create_sequence #creates sequence for either duplicates being included or excluded
     if @create_game.dups==false
       @sequence=colors.shuffle.take(@create_game.pegs)
     else
-      @sequence=Array.new(@create_game.pegs) {colors.sample}
+      @sequence=Array.new(@create_game.pegs) {colors.sample} #sequence for duplicates
     end
   end
-  def guess
+  
+  def guess #function that takes inut from the user as a guess of the sequence.
     puts "Enter guess, seperated by spaces:"
     @guess=gets.chomp.split(' ')
     @all_guesses << @guess
   end
-  def sequence_guessed?
+  def sequence_guessed? #checks if the sequence was guessed correctly, returning true if it was.
     @all_guesses.include?(@sequence)
   end
 
-  def max_guesses_reached?
+  def max_guesses_reached? #checks if the max number of guesses was reached, returning true if it was.
     @all_guesses.length >= @max_guesses
   end
 end
 
 #The Display class contains all of the functions necessary to show the results of a guesses and previous guesses while checking the guesses goes hand in hand woth Play_Game.
 class Display
-  def initialize(play_game)
+  def initialize(play_game) #initializes using the play_game instance while creating an array for all checks for each guess
     @play_game=play_game
-    @checks=[]
+    @checks=[] #will store the corresponding checks for each guess in the correct order.
   end
   
-  def guess_check(sequence, guess)
+  def guess_check(sequence, guess) #checks each guessagainst the sequence and returns the results.
     results=''
     guess.each_with_index do |color,index|
       if sequence[index]==color
-        results += "*"
+        results += "*" # shown if the right color is in the right spot
       end
     end
-    numChecks=results.size
-    period_num=sequence.intersection(guess).length-numChecks
+    numChecks=results.size #gets size of the results after adding the *'s
+    period_num=sequence.intersection(guess).length-numChecks #subtracts the number of *' from the number of .'s to avoid double counting
     period_num.times do
-      results += '.'
+      results += '.' #shown if the right color is in the guess.
     end
-    remaining=sequence.length-results.length
+    remaining=sequence.length-results.length #fills the rest of the results with x's until its the same size as sequence (= to number of pegs)
     remaining.times do
-      results += 'x'
+      results += 'x' #shown if the wrong color is included in the guess.
     end
-    @checks<<results
+    @checks<<results #appends results to the checks array
   end
   
-  def show
+  def show #prints out the previous guesses and their corresponding results to the terminal.
     @play_game.all_guesses.each_with_index do |guess, index|
       print "Guess #{index + 1}: #{guess.join(' ')}  "  # Print the guess
-      puts "Check: #{@checks[index]}"
+      puts "Check: #{@checks[index]}" #Prints results of the guess
     end
   end
 end
